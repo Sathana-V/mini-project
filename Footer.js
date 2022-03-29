d697e8cfc62b6986eb79e977ce3eb259e61a894f
Sathana-V <69391766+Sathana-V@users.noreply.github.com>
2022-03-29 22:55:44 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,96 @@
+class Footer extends HTMLElement {
+    constructor() {
+      super();
+    }
+  
+    connectedCallback() {
+      this.innerHTML = `
+        
+      <div class="d-flex flex-column h-100">
+      <footer class="w-100 py-4 flex-shrink-0">
+
+          <div class="container py-4">
+            <div class="row footer-heading">
+              <div class="col-lg-6" style="margin-top:-2%">
+                <h2>Do you have any questions?</h2>
+                <h3 style="color:#fa5255">We're here for you!</h3>
+              </div>
+              <div class="col-lg-3">
+                <h5><span><i class="fa fa-phone icons"></i></span>&nbsp;&nbsp;+49 (0) 30 2204 5903</h5>
+              </div>
+              <div class="col-lg-3">
+                <h5><span><i class="fa fa-envelope icons"></i></span>&nbsp;&nbsp;info@pitchguru.com</h5>
+            
+              </div>
+            </div>
+            <div class="row" style="height:70px"></div>
+              <div class="row gy-4 gx-5 mt-3" style="margin-top:8%">
+                  <div class="col-lg-4 col-md-6">
+                  
+                      <img usemap="#pitch-group-links" src="images/pitch-group.png" alt="logo" width="200" height="100"/>
+                      <map name = "pitch-group-links">
+                        <area shape = "rect" coords = "14,84,36,106" 
+                           href = "https://google.com" alt="linked-in"  />
+                        
+                        <area shape = "rect" coords = "61,84,88,107" alt="medoium" 
+                           href = "https://medium.com/" target = "_self" />
+                           <area shape = "rect" coords = "104,83,143,106" 
+                           href = "https://instagram.com" alt="linked-in"  />
+                           <area shape = "rect" coords = "158,85,190,106" 
+                           href = "https://facebook.com" alt="linked-in"  />
+                     </map>
+                    </div>
+                  <div class="col-lg-2 col-md-6">
+                     <strong><h5 class="text-black mb-3">company </h5></strong> 
+                      <ul class="list-unstyled text-muted">
+                          <li><a href="#">about us</a></li>
+                          <li><a href="#">Customers</a></li>
+                          <li><a href="#">Trust & Security</a></li>
+                          <li><a href="#">Start Order</a></li>
+                      </ul>
+                  </div>
+                  <div class="col-lg-1"></div>
+                  <div class="col-lg-2 col-md-6">
+                      <strong><h5 class="text-black mb-3">resources</h5></strong>
+                      <ul class="list-unstyled text-muted">
+                          <li><a href="#">white paper</a></li>
+                          <li><a href="#">Content sheet</a></li>
+                          <li><a href="#">101 investor questions</a></li>
+                      </ul>
+                  </div>
+                  <div class="col-lg-1"></div>
+                  <div class="col-lg-2 col-md-6">
+                    <strong><h5 class="text-black mb-3">Information</h5></strong>
+                    <ul class="list-unstyled text-muted">
+                        <li><a href="#">Faq</a></li>
+                    </ul>
+                </div>
+                <div class="payments">
+                 
+                  <img class="payment-img" src="images/payment.png" alt="">
+                </div>
+          </div>
+          <hr style="height:1px;border:10px;color:#333;background-color:#333;" />
+          <div class="container copyrights">
+            <div class="row">
+              <div class="col-lg-8">
+                <p>Copyright 2021 © PitchGuru</p>
+              </div>
+              <div class="col-lg-4 copyrights-group">
+
+                <a href="#" >Condition</a>
+                <a href="#">imprint</a>
+                <a href="#">Privacy</a>
+              </div>
+             
+            </div>
+          </div>
+      </footer>
+  </div>
+        
+        
+      `;
+    }
+  }
+  
+  customElements.define('footer-component', Footer);
---
+++ Header.js
@@ -0,0 +1,78 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+      
+      
+    <nav class="navbar navbar-expand-lg  fixed-top navbar-light bg-light">
+    <div class="container-fluid">
+      <img src="images/pitch.png" alt="" width="170px" height="50px">
+     
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" style="margin-left:4%" id="navbarSupportedContent">
+        <ul class="navbar-nav d-flex justify-content-around me-auto mb-2 mb-lg-0 nav-fill">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="/about.html">About us</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Products
+            </a>
+            <ul class="dropdown-menu nav-fill" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">pitch decks</a></li>
+              <li><a class="dropdown-item" href="#">sales decks</a></li>
+              <li><a class="dropdown-item" href="#">finance decks</a></li>
+              <li><a class="dropdown-item" href="#">corporate decks</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Enterprise</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link">Templates</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+             Resources
+            </a>
+            <ul class="dropdown-menu nav-fill" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">white paper</a></li>
+              <li><a class="dropdown-item" href="#">content sheet</a></li>
+              <li><a class="dropdown-item" href="#">101 investor questions</a></li>
+            </ul>
+          </li>
+        </ul>
+          <!--<div class="d-none d-lg-flex  align-items-baseline ">
+            <p class="text white mx-4">
+                <span class="mx-2 phone-icon ">
+                        <i class="fas fa fa-phone fa-fw"></i>8765439026</span>
+            </p>
+           </div>-->
+           <div class="header__wrapper-phone" >
+             <!--<a class="header__phone" href="tel:+493022045903">
+              <i class="fa-solid fa-phone"></i>
+               <i class="icon-font icon-phone cv--click--phone-number--header-nav"></i>
+               <span>93672982783</span>
+              </a>-->
+              <p><i class="fa fa-phone" aria-hidden="true"></i>  0120-6619540</p><br>
+            </div>
+           <div   class="nav-right1">
+           <form  class="container-fluid justify-content-start ">
+            <button type="button"  class=" navbutton" style="background-color: #fa5255; color:white;font-size: 20px;">Order now</button>
+          </form>
+        </div>
+    </div>
+    </div>
+  </nav>
+      
+      
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
