9c390d860f2683a8edda67b3f22760109721f100
sathana-v <sathanav.19cse@kongu.edu>
2022-04-16 03:27:55 +0530

    added routes

--- Header.js
+++ Header.js
@@ -28,25 +28,25 @@ class Header extends HTMLElement {
             </a>
             <ul class="dropdown-menu nav-fill" aria-labelledby="navbarDropdown">
               <li><a class="dropdown-item" href="pitch-deck.html">pitch decks</a></li>
-              <li><a class="dropdown-item" href="#">sales decks</a></li>
-              <li><a class="dropdown-item" href="#">finance decks</a></li>
-              <li><a class="dropdown-item" href="#">corporate decks</a></li>
+              <li><a class="dropdown-item" href="sales-deck.html">sales decks</a></li>
+              <li><a class="dropdown-item" href="finance-deck.html">finance decks</a></li>
+              <li><a class="dropdown-item" href="cooperate.html">corporate decks</a></li>
             </ul>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/enterprise.html">Enterprise</a>
+            <a class="nav-link" href="enterprise.html">Enterprise</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link">Templates</a>
+          <a class="nav-link" href="/templates.html">Templates</a>
           </li>
           <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
              Resources
             </a>
             <ul class="dropdown-menu nav-fill" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">white paper</a></li>
-              <li><a class="dropdown-item" href="#">content sheet</a></li>
-              <li><a class="dropdown-item" href="#">101 investor questions</a></li>
+              <li><a class="dropdown-item" href="resource1.html">white paper</a></li>
+              <li><a class="dropdown-item" href="resource2.html">content sheet</a></li>
+              <li><a class="dropdown-item" href="resource3.html">101 investor questions</a></li>
             </ul>
           </li>
         </ul>
@@ -66,7 +66,7 @@ class Header extends HTMLElement {
             </div>
            <div   class="nav-right1">
            <form  class="container-fluid justify-content-start ">
-            <button type="button"  class=" navbutton" style="background-color: #fa5255; color:white;font-size: 20px;">Order now</button>
+            <button type="button"  class="navbutton" style="background-color: #fa5255; color:white;font-size: 20px;">Order now</button>
           </form>
         </div>
     </div>
